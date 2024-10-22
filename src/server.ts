1b5e1ec02bb6b4ec4d7926a091ca534aa41e2277
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-10-22 12:26:13 +0600

    unhandle rejection and uncaught rejection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app";
 import mongoose from "mongoose";
 import config from "./app/config";
+import { Server } from "http";
 
+let server: Server;
 async function main() {
   const res = await mongoose.connect(config.db_url as string);
   console.log(res.connection.name);
@@ -9,4 +11,19 @@ async function main() {
     console.log(`Example app listening on port ${config.port}`);
   });
 }
-main().catch((err) => console.log(err));
+main();
+
+process.on("unhandledRejection", (err) => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
